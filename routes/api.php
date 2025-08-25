2dbe628bdcd9c41136818e66ff4d4d9c5bfd7ca3
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-25 19:39:28 -0300

    feat: add mysql

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function (Request $req) {
+    throw new ErrorException('houve um ping');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
