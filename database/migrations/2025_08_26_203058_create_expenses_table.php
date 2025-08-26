ccc424b0e85fdfa8df5ceb52b4c64eb1ee297989
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-26 19:43:12 -0300

    feat: create migrations

---
+++ database/migrations/2025_08_26_203058_create_expenses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 250);
+            $table->float('value');
+            $table->softDeletes();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignIdFor(Category::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
