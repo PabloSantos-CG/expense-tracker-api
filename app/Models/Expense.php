5f84b8168c7ed1f53449db6c8a8c52975e6448b7
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-26 19:52:36 -0300

    feat: relations

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
-    //
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
