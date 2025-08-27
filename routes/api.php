e0334d71a96eed4b7f7face406073d57b08ccafa
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-27 19:29:13 -0300

    refactor: generate form request and controller with artisan

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ Route::get('/ping', function (Request $req) {
     throw new ErrorException('houve um ping');
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
