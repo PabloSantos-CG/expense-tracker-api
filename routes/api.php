6b378eae7da0d2fbcb47af55f1e07e0b98d80201
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-27 20:22:33 -0300

    refactor: add authcontroller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\UserController;
@@ -10,25 +11,25 @@ Route::get('/ping', function (Request $req) {
     throw new ErrorException('houve um ping');
 });
 
-Route::get('/user/login', [UserController::class, 'logIn']);
-Route::get('/user/logout', [UserController::class, 'logOut']);
+Route::get('/user/login', [AuthController::class, 'logIn']);
+Route::get('/user/logout', [AuthController::class, 'logOut']);
 
-Route::get('/users', UserController::class, 'index');
-Route::post('/user', UserController::class, 'store');
-Route::get('/user/{id}', UserController::class, 'show');
-Route::put('/user/{id}', UserController::class, 'update');
-Route::delete('/user/{id}', UserController::class, 'destroy');
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/user', [UserController::class, 'store']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::put('/user/{id}', [UserController::class, 'update']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
-Route::get('/categories', CategoryController::class, 'index');
-Route::post('/category', CategoryController::class, 'store');
-Route::get('/category/{id}', CategoryController::class, 'show');
-Route::put('/category/{id}', CategoryController::class, 'update');
-Route::delete('/category/{id}', CategoryController::class, 'destroy');
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/category', [CategoryController::class, 'store']);
+Route::get('/category/{id}', [CategoryController::class, 'show']);
+Route::put('/category/{id}', [CategoryController::class, 'update']);
+Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
 
-Route::get('/category/expenses', ExpenseController::class, 'index');
-Route::post('/category/expense', ExpenseController::class, 'store');
-Route::get('/category/expense/{id}', ExpenseController::class, 'show');
-Route::put('/category/expense/{id}', ExpenseController::class, 'update');
-Route::delete('/category/expense/{id}', ExpenseController::class, 'destroy');
+Route::get('/category/expenses', [ExpenseController::class, 'index']);
+Route::post('/category/expense', [ExpenseController::class, 'store']);
+Route::get('/category/expense/{id}', [ExpenseController::class, 'show']);
+Route::put('/category/expense/{id}', [ExpenseController::class, 'update']);
+Route::delete('/category/expense/{id}', [ExpenseController::class, 'destroy']);
 
 
