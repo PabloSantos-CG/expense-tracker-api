c890f096319f89befaead592e7c65e4165bd9fd5
PabloSantos-CG <comercial-pablo@outlook.com>
2025-08-27 19:56:00 -0300

    feat: create category routes and expense routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,9 +14,21 @@ Route::get('/user/login', [UserController::class, 'logIn']);
 Route::get('/user/logout', [UserController::class, 'logOut']);
 
 Route::get('/users', UserController::class, 'index');
-Route::post('/users', UserController::class, 'store');
+Route::post('/user', UserController::class, 'store');
 Route::get('/user/{id}', UserController::class, 'show');
 Route::put('/user/{id}', UserController::class, 'update');
 Route::delete('/user/{id}', UserController::class, 'destroy');
+
+Route::get('/categories', CategoryController::class, 'index');
+Route::post('/category', CategoryController::class, 'store');
+Route::get('/category/{id}', CategoryController::class, 'show');
+Route::put('/category/{id}', CategoryController::class, 'update');
+Route::delete('/category/{id}', CategoryController::class, 'destroy');
+
+Route::get('/category/expenses', ExpenseController::class, 'index');
+Route::post('/category/expense', ExpenseController::class, 'store');
+Route::get('/category/expense/{id}', ExpenseController::class, 'show');
+Route::put('/category/expense/{id}', ExpenseController::class, 'update');
+Route::delete('/category/expense/{id}', ExpenseController::class, 'destroy');
 
 
